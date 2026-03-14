b31c2c683f408b15964ac1e14688deeff9d94845
赵芷若 <1512619344@qq.com>
2026-03-14 13:58:06 +0800

    feat: use protobuf for inter-node communication

--- geecache/http.go
+++ geecache/http.go
@@ -3,12 +3,15 @@ package geecache
 import (
 	"fmt"
 	"geecache/geecache/consistenthash"
+	pb "geecache/geecache/geecachepb"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"strings"
 	"sync"
+
+	"google.golang.org/protobuf/proto"
 )
 
 const (
@@ -67,32 +70,42 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	body, err := proto.Marshal(&pb.Response{Value: view.ByteSlice()})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	w.Header().Set("Content-Type", "application/octet-stream")
-	w.Write(view.ByteSlice())
+	w.Write(body)
 }
 
-func (h *httpGetter) Get(group string, key string) ([]byte, error) {
+func (h *httpGetter) Get(in *pb.Request, out *pb.Response) error {
 	u := fmt.Sprintf(
 		"%v%v%v",
 		h.baseURL,
-		url.QueryEscape(group),
-		url.QueryEscape(key),
+		url.QueryEscape(in.GetGroup()),
+		url.QueryEscape(in.GetKey()),
 	)
 	res, err := http.Get(u)
 	if err != nil {
-		return nil, err
+		return nil
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("server returned: %v", res.Status)
+		return nil
 	}
 
 	bytes, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return nil, fmt.Errorf("reading response body: %v", err)
+		return nil
 	}
-	return bytes, nil
+
+	if err = proto.Unmarshal(bytes, out); err != nil {
+		return fmt.Errorf("decoding response body: %v", err)
+	}
+
+	return nil
 }
 
 func (p *HTTPPool) Set(peers ...string) {
--- geecache/peers.go
+++ geecache/peers.go
@@ -1,5 +1,7 @@
 package geecache
 
+import pb "geecache/geecache/geecachepb"
+
 // 用于根据传入的 key 选择相应节点 PeerGetter。
 type PeerPicker interface {
 	PickPeer(key string) (peer PeerGetter, ok bool)
@@ -7,5 +9,5 @@ type PeerPicker interface {
 
 // 从对应 group 查找缓存值 对应于上述流程中的 HTTP 客户端。
 type PeerGetter interface {
-	Get(group string, key string) ([]byte, error)
+	Get(in *pb.Request, out *pb.Response) error
 }
